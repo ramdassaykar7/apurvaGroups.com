a1c34e524fc03fafa66954669fe481f52877f91a
krishnakushare <krishkusharekk@gmail.com>
2018-09-09 21:47:51 +0530

    navnintro

--- apurvaGroups/src/app/app.module.ts
+++ apurvaGroups/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -7,6 +8,16 @@ import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { SliderComponent } from './main/slider/slider.component';
 import { IntroComponent } from './main/intro/intro.component';
+import { CardsComponent } from './main/cards/cards.component';
+import { GallaryComponent } from './main/gallary/gallary.component';
+
+
+const appRoutes: Routes=[
+  { path: 'app-main', component: MainComponent },
+  { path: 'app-intro', component: IntroComponent },
+
+  { path:'', redirectTo: '', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
@@ -15,10 +26,13 @@ import { IntroComponent } from './main/intro/intro.component';
     FooterComponent,
     MainComponent,
     SliderComponent,
-    IntroComponent
+    IntroComponent,
+    CardsComponent,
+    GallaryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
