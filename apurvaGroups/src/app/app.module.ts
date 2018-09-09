5dc7b3faa8883fa057771e446b8daf82ca3aa712
krishnakushare <krishkusharekk@gmail.com>
2018-09-09 17:34:02 +0530

    intro

--- apurvaGroups/src/app/app.module.ts
+++ apurvaGroups/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { SliderComponent } from './main/slider/slider.component';
+import { IntroComponent } from './main/intro/intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainComponent,
+    SliderComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule
